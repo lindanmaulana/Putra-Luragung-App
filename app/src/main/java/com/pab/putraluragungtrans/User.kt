d13bdd02e3efeda9b4f246dc9bfc669517866501
Lindan Maulana <lindanstemga@gmail.com>
2025-12-28 17:33:17 +0700

    fix: block register user if email already exists

--- app/src/main/java/com/pab/putraluragungtrans/User.kt
+++ app/src/main/java/com/pab/putraluragungtrans/User.kt
@@ -1,9 +1,13 @@
 package com.pab.putraluragungtrans
 
 import androidx.room.Entity
+import androidx.room.Index
 import androidx.room.PrimaryKey
 
-@Entity
+@Entity(
+    tableName = "User",
+    indices = [Index(value = ["email"], unique = true)]
+)
 data class User(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
     val firstName: String,
